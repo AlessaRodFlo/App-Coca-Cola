1275d951f4975775cf799c33c49c500dfd8dc1b1
alessa223 <alessarodflo@micorreo.upp.edu.mx>
2026-01-28 15:57:13 -0600

    Update API baseUrl for production

--- frontend/lib/config/api.dart
+++ frontend/lib/config/api.dart
@@ -1,4 +1,18 @@
 class ApiConfig {
-  // OJO: en Android emulator, localhost del host = 10.0.2.2
-  static const String baseUrl = "http://localhost:3000/api";
+  // Cambia a false si quieres usar el backend local
+  static const bool isProd = true;
+
+  // Backend en Render (producción)
+  static const String _prod = "https://app-coca-cola.onrender.com/api";
+
+  // Backend local (desarrollo)
+  // Si corres en Chrome (web): localhost sirve
+  static const String _localWeb = "http://localhost:3000/api";
+
+  // Si corres en Android Emulator: localhost del host = 10.0.2.2
+  //static const String _localAndroid = "http://10.0.2.2:3000/api";
+
+  // URL final
+  static const String baseUrl = isProd ? _prod : _localWeb;
+  // Si vas a probar en Android Emulator, cambia _localWeb por _localAndroid
 }
